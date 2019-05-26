be0a03cf9b92def5363431c770d74a85d6062663
wzy2006 <3450354617@qq.com>
2019-05-26 19:44:32 +0800

    v1.1beta

--- farrangement.py
+++ farrangement.py
@@ -13,7 +13,7 @@ def CEF(path):
             if os.path.getsize(file) == 0:  # 文件大小为0
                 os.remove(file)  # 删除这个文件
                 
-print ("Farrangement 文件整理 [版本 0.9beta]")
+print ("Farrangement 文件整理 [版本 1.1beta]")
 print ("遵循GPLv3协议")
 print ("作者: wzy2006 博客: wzy2006.github.io")
 try:
@@ -26,26 +26,26 @@ try:
     os.mkdir("其他")
 except BaseException:
     i=0
-path="C:\\Users\\Administrator\\Desktop\\"
+path=os.path.join(os.path.expanduser("~"), 'Desktop')+"\\"
 print("\n开始整理")
 lis=list(os.walk(path))[0][2]
 for i in lis:
     ftype=(os.path.splitext(i)[1])
     if ftype=='.doc' or ftype == '.docx' or ftype == '.wps' or ftype =='.et' or ftype =='.xls' or ftype =='.xlsx' or ftype=='.ppt' or ftype =='.pptx' or ftype=='.dps' or ftype=='.txt'or ftype==".pdf":
-        fm.move(path+i,"/文档/"+i)
+        fm.move(path+i,os.path.join(os.path.join(path,"文档"),i))
     elif ftype=='.jpg' or ftype==".jpeg" or ftype==".png" or ftype==".gif" or ftype==".bmp" or ftype==".pcx" or ftype==".tiff":
-        fm.move(path+i,"./图片/"+i)
+        fm.move(path+i,os.path.join(os.path.join(path,"图片"),i))
     elif ftype==".zip" or ftype==".rar" or ftype==".gz" or ftype==".tar" or ftype==".7z" or ftype==".iso":
-         fm.move(path+i,"./压缩/"+i)
+         fm.move(path+i,os.path.join(os.path.join(path,"压缩"),i))
     elif ftype==".mp4" or ftype==".mkv" or ftype==".mov" or ftype==".rm" or ftype==".rmvb" or ftype==".wmv" or ftype==".avi":
-        fm.move(path+i,"./视频/"+i)
+        fm.move(path+i,os.path.join(os.path.join(path,"视频"),i))
     elif ftype==".mp3" or ftype==".wmv" or ftype==".wav" or ftype==".flac":
-        fm.move(path+i,"./音乐/"+i)
-    elif ftype==".java" or ftype==".c" or ftype==".cpp" or ftype==".exe" or ftype==".dll" or ftype==".lib" or ftype==".dsp" or ftype==".dsw" or ftype==".cs" or ftype==".asp" or ftype==".aspx" or ftype==".php" or ftype==".jsp" or ftype==".go" or ftype==".py" or ftype==".pyc" or ftype==".asm" or ftype==".vbs" or ftype==".bat" or ftype==".cmd":
-        fm.move(path+i,"./代码/"+i)
-    else:
-        fm.move(path+i,"./其他/"+i)
-CEF('C:\\Users\\Administrator\\Desktop\\')
+        fm.move(path+i,os.path.join(os.path.join(path,"音乐"),i))
+    elif ftype==".java" or ftype==".c" or ftype==".cpp" or ftype==".dll" or ftype==".lib" or ftype==".dsp" or ftype==".dsw" or ftype==".cs" or ftype==".asp" or ftype==".aspx" or ftype==".php" or ftype==".jsp" or ftype==".go" or ftype==".py" or ftype==".pyc" or ftype==".asm" or ftype==".vbs" or ftype==".bat" or ftype==".cmd":
+        fm.move(path+i,os.path.join(os.path.join(path,"代码"),i))
+    elif ftype!=".lnk":
+        fm.move(path+i,os.path.join(os.path.join(path,"其他"),i))
+CEF(path)
 
 
 print("完成!")
